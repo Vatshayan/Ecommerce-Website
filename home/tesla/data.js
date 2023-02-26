3b787ae11b99a653847198c04d86360782434528
81CuongVn <alan45walker12@gmail.com>
2023-02-26 18:00:07 +0700

    update

---
+++ home/tesla/data.js
@@ -0,0 +1,156 @@
+export const cars = [
+  {
+    id: "e1",
+    name: "Model S",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("./assets/home/ModelS.jpeg"),
+    screen: "ModelSScreen",
+  },
+  {
+    id: "e2",
+    name: "Model X",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("./assets/home/ModelX.jpeg"),
+    screen: "ModelXScreen",
+  },
+  {
+    id: "e3",
+    name: "Model Y",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("./assets/home/ModelY.jpeg"),
+    screen: "ModelYScreen",
+  },
+  {
+    id: "e4",
+    name: "Model 3",
+    tagline: "Order Online for",
+    taglineCTA: "Touchless Delivery",
+    image: require("./assets/home/Model3.jpeg"),
+    screen: "Model3Screen",
+  },
+];
+
+export const detailsList = [
+  {
+    id: "e1",
+    name: "Model S",
+    image: require("./assets/customOrderPage/modelSImage.png"),
+    range: "652km",
+    topSpeed: "250km/h",
+    zeroToHundred: "3.2sec",
+    rangeHeader: "Range (EPA est.)",
+    topSpeedHeader: "Top Speed",
+    zeroToHundredHeader: "0-100 km/h",
+    delivery: "Est. Delivery: March-April",
+  },
+  {
+    id: "e2",
+    name: "Model X",
+    image: require("./assets/customOrderPage/modelXImage.png"),
+    range: "580km",
+    topSpeed: "250km/h",
+    zeroToHundred: "3.9sec",
+    rangeHeader: "Range (EPA est.)",
+    topSpeedHeader: "Top Speed",
+    zeroToHundredHeader: "0-100 km/h",
+    delivery: "Est. Delivery: March-April",
+  },
+  {
+    id: "e3",
+    name: "Model Y",
+    image: require("./assets/customOrderPage/modelYImage.png"),
+    range: "525km",
+    topSpeed: "217km/h",
+    zeroToHundred: "5.0sec",
+    rangeHeader: "Range (EPA est.)",
+    topSpeedHeader: "Top Speed",
+    zeroToHundredHeader: "0-100 km/h",
+    delivery: "Est. Delivery: March",
+  },
+  {
+    id: "e4",
+    name: "Model 3",
+    image: require("./assets/customOrderPage/model3Image.png"),
+    range: "568km",
+    topSpeed: "233km/h",
+    zeroToHundred: "4.4sec",
+    rangeHeader: "Range (EPA est.)",
+    topSpeedHeader: "Top Speed",
+    zeroToHundredHeader: "0-100 km/h",
+    delivery: "Est. Delivery: December",
+  },
+];
+
+export const videoList = [
+  {
+    id: "f1",
+    videoClip: require("./assets/videos/autopilot-tesla.mp4"),
+  },
+  {
+    id: "f2",
+    videoClip: require("./assets/videos/parking.mp4"),
+  },
+  {
+    id: "f3",
+    videoClip: require("./assets/videos/parallel.mp4"),
+  },
+];
+
+export const inventory = [
+  {
+    id: "d1",
+    name: "Model S",
+    image: require("./assets/customOrderPage/modelSImage.png"),
+    price: "$82,990",
+  },
+  {
+    id: "d2",
+    name: "Model X",
+    image: require("./assets/customOrderPage/modelXBlack.png"),
+    price: "$93,190",
+  },
+  {
+    id: "d3",
+    name: "Model 3",
+    image: require("./assets/customOrderPage/model3Red.png"),
+    price: "$34,190",
+  },
+  {
+    id: "d4",
+    name: "Model S",
+    image: require("./assets/customOrderPage/modelSRed.png"),
+    price: "$82,990",
+  },
+  {
+    id: "d5",
+    name: "Model X",
+    image: require("./assets/customOrderPage/modelXGrey.png"),
+    price: "$93,190",
+  },
+  {
+    id: "d6",
+    name: "Model 3",
+    image: require("./assets/customOrderPage/model3Blue.png"),
+    price: "$34,190",
+  },
+  {
+    id: "d7",
+    name: "Model Y",
+    image: require("./assets/customOrderPage/modelYBlack.png"),
+    price: "$48,190",
+  },
+];
+
+export const choice = [
+  {
+    id: "s1",
+    title: "Add",
+  },
+  {
+    id: "s2",
+    title: "Don't Add",
+  },
+];
